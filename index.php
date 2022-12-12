64a1f6dd955332c61ea9a951b92f4dc6490564d9
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-12-12 15:33:40 +0100

    end bonus

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
 <?php
 include_once __DIR__ . '/models/movie.php';
-$starwars = new Movie("star wars", "fantascentifico", "5 ");
-var_dump($starwars);
-?>
+$movie1 = new Movie("Metropolis", "Fritz Lang", 1927, 8.2, array("Drammatico", "Fantascienza"));
+$movie2 = new Movie("GUERRE STELLARI V - L'IMPERO COLPISCE ANCORA", "Ivrin Kershner", 1980, 8.1, array("Azione", "Fantascienza", "Fantasy", "Famiglia"));
 
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+// Stampa dei dettagli dei film
+$movie1->printDetails();
+echo "<br>";
+$movie2->printDetails();
--- models/movie.php
+++ models/movie.php
@@ -1,13 +1,34 @@
 <?php
 class Movie
 {
-    public $nome;
-    public $genere;
+    // Variabili d'istanza
+    public $title;
+    public $director;
+    public $year;
     public $rating;
-    function __construct($_nome, $_genere, $_rating)
+    public $generes = array();
+
+    // Costruttore
+    public function __construct($_title, $_director, $_year, $_rating, $_generes)
     {
-        $this->nome = $_nome;
-        $this->genere = $_genere;
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->year = $_year;
         $this->rating = $_rating;
+        $this->generes = $_generes;
+    }
+
+    // Metodo per stampare i dettagli del film
+    public function printDetails()
+    {
+        echo "Title: " . $this->title . "<br>";
+        echo "Director: " . $this->director . "<br>";
+        echo "Year: " . $this->year . "<br>";
+        echo "Voto: " . $this->rating . "<br>";
+        echo "Generes : ";
+        foreach ($this->generes as $genere) {
+            echo $genere . " , ";
+        }
+        echo "<br>";
     }
 }
